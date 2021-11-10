fba1c27c449929bd57c4bf5a08496fce4fa7ec15
Mark Goho <markgoho@gmail.com>
2021-11-10 09:43:55 -0500

    export additional mock

--- test/mocks/index.ts
+++ test/mocks/index.ts
@@ -1,4 +1,5 @@
 export * from './cucumberReport.mock';
+export * from './cucumberReport2.mock';
 export * from './baseSteps.mock';
 export * from './backgroundElement.mock';
 export * from './scenarioElement.mock';
